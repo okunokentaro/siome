34e6dced162a026f48b09d03455df531e6fb1eb6
OKUNOKENTARO <okunokentaro@me.com>
2015-06-21 20:54:24 +0900

    refactor(constants): carve out to constants

--- app/app.js
+++ app/app.js
@@ -2,7 +2,8 @@ import angular from 'angular';
 import 'firebase';
 import 'angularfire';
 
-const APP_NAME = 'ika_yoroshiku';
-const APP_PLUGINS = ['firebase'];
+import {APP_NAME, APP_PLUGINS} from './constants';
 
 angular.module(APP_NAME, APP_PLUGINS);
+
+import './auth';
---
+++ app/constants.js
@@ -0,0 +1,3 @@
+export const appName = 'ika_yoroshiku';
+export const plugins = ['firebase'];
+export const firebaseUrl = 'https://ika-yoroshiku.firebaseio.com/';
