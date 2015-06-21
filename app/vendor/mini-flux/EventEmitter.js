e662dd51e12cf92e612fa14c9e225c012e3219fb
OKUNOKENTARO <okunokentaro@me.com>
2015-06-21 23:40:01 +0900

    use EventEmitter from mini- flux
    https://github.com/azu/mini-flux/blob/master/src/EventEmitter.js

---
+++ app/vendor/mini-flux/EventEmitter.js
@@ -0,0 +1,37 @@
+// https://github.com/azu/mini-flux/blob/master/src/EventEmitter.js
+// LICENSE : MIT
+"use strict";
+/*
+    Simple EventEmitter
+ */
+export default class EventEmitter {
+    constructor() {
+        this._handlers = {};
+    }
+
+    on(type, handler) {
+        if (typeof this._handlers[type] === 'undefined') {
+            this._handlers[type] = [];
+        }
+
+        this._handlers[type].push(handler);
+    }
+
+    emit(type, data) {
+        var handlers = this._handlers[type] || [];
+        for (var i = 0; i < handlers.length; i++) {
+            var handler = handlers[i];
+            handler.call(this, data);
+        }
+    }
+
+    off(type, handler) {
+        var handlers = this._handlers[type] || [];
+        for (var i = 0; i < handlers.length; i++) {
+            var ownHandler = handlers[i];
+            if (ownHandler === handler) {
+                handlers.splice(i, 1);
+            }
+        }
+    }
+}
