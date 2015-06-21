d3b6b93e4f57b5b48decad66c536dfa7fef7f9b4
OKUNOKENTARO <okunokentaro@me.com>
2015-06-21 21:30:10 +0900

    feat(ika-app): update

--- app/directives/ika-app.js
+++ app/directives/ika-app.js
@@ -1,16 +1,30 @@
 import angular from 'angular';
 import {appName} from '../constants';
 
+const directiveName = 'ikaApp';
+
+class IkaAppController {
+  constructor() {
+    this.title = 'イカよろ';
+    this.description = '『イカよろ』はTwitterアカウントとイカID（ニンテンドーネットワークID）をまとめて登録・検索ができるサービスです！イカ、よろしく〜〜〜';
+  }
+}
+
 const template = `
-<h1>hello</h1>
+<h1>{{${directiveName}.title}}</h1>
+<div>
+  <p>{{${directiveName}.description}}</p>
+</div>
 `;
 
 const ddo = () => {
   return {
     restrict: 'E',
     scope: {},
-    template: template
+    template: template,
+    controller: IkaAppController,
+    controllerAs: directiveName
   };
 };
 
-angular.module(appName).directive('ikaApp', ddo);
+angular.module(appName).directive(directiveName, ddo);
